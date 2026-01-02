33428a17ceaaf4b01271c56fffc77735d68920a2
arisofia <jenineferrd@yhoo.com>
2026-01-02 17:18:27 +0100

    Fix ingestion test import path

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pytest
 
-from python.pipeline.ingestion import UnifiedIngestion
+from src.pipeline.data_ingestion import UnifiedIngestion
 
 
 def test_ingest_csv(tmp_path, minimal_config):
